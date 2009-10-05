1c7dd6474e85e961b2e5e291ee40ce38b303fc73
litagratrix <litagratrix@acffc2da-a247-11de-8af6-edd33a9b1062>
2009-10-05 06:55:54 +0000

    Changed Visit Controller to a Simple Form Controller, and also tried to add validation for Visit entries to the dB.

--- trunk/serengeti/src/main/java/com/playserengeti/dao/LocationDao.java
+++ trunk/serengeti/src/main/java/com/playserengeti/dao/LocationDao.java
@@ -5,47 +5,53 @@ import java.util.Collection;
 import com.playserengeti.domain.Location;
 
 public interface LocationDao {
-	
+
 	/**
 	 * Create a new location.
-	 * 
+	 *
 	 * @param location to commit
 	 * @return the location id of the newly inserted location
 	 */
 	public Integer insertLocaion(Location loc);
-	
+
 	/**
 	 * Update the location in storage.
-	 * 
+	 *
 	 * @param location to update
 	 */
 	public void updateLocation(Location loc);
-	
+
 	/**
 	 * Delete a location from storage.
-	 * 
+	 *
 	 * @param id location id of the location to delete
 	 */
 	public void deleteLocation(Integer id);
-	
+
 	/**
 	 * Get all locations from storage.
 	 */
 	public Collection<Location> getAllLocations();
-	
+
 	/**
 	 * Get a location from storage.
-	 * 
+	 *
 	 * @param id location id of the location to fetch
 	 * @return the fetched location or null if none exist with that id
 	 */
 	public Location getLocationById(Integer id);
-	
+
 	/**
 	 * Get a location from storage.
-	 * 
+	 *
 	 * @param loc location name
 	 * @return the fetched location
 	 */
 	public Location getLocationByLocationName(String loc);
+
+	/*
+	 * Check the existance of a locaitionId
+	 */
+	public boolean locationExists(Integer locationId);
+
 }
--- trunk/serengeti/src/main/webapp/WEB-INF/tiles/visitCreate.jsp
+++ trunk/serengeti/src/main/webapp/WEB-INF/tiles/visitCreate.jsp
@@ -7,13 +7,6 @@
     </head>
 
     <body>
-    	<c:if test='${ !empty userId && !empty locationId }'>
-
-    		<p>Stub page for adding a visit from <strong><c:out value="${userId}"/></strong>
-    		to location <strong><c:out value="${locationId}"/></strong>...</p>
-    	</c:if>
-
-        <c:if test='${ empty userId || empty locationId }'>
     	<p>Check in UserId at LocationId</p>
         <p>
 			<form method="get" action="">
@@ -22,6 +15,5 @@
 				<input type="submit" value="Check In!"/>
 			</form>
         </p>
-        </c:if>
     </body>
 </html>
