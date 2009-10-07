8b96b85026359c69702b15f83b6e4f240d0a3952
loren.abrams <loren.abrams@acffc2da-a247-11de-8af6-edd33a9b1062>
2009-10-07 17:10:39 +0000

    Implemented user delete, including some cool javascript.

--- trunk/serengeti/src/main/java/com/playserengeti/controller/UserDeleteController.java
+++ trunk/serengeti/src/main/java/com/playserengeti/controller/UserDeleteController.java
@@ -4,16 +4,21 @@
 
 package com.playserengeti.controller;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractCommandController;
+import org.springframework.web.servlet.mvc.SimpleFormController;
 
+import com.playserengeti.domain.User;
 import com.playserengeti.service.UserService;
 
-public class UserDeleteController extends AbstractCommandController {
+public class UserDeleteController extends SimpleFormController {
 
     private UserService service;
 
@@ -21,21 +26,36 @@ public class UserDeleteController extends AbstractCommandController {
         this.service = service;
     }
 
+    /**
+     * Method provides the list of all users to the form view.
+     */
     @Override
-    protected ModelAndView handle(HttpServletRequest request,
-            HttpServletResponse response, Object commandObject,
-            BindException errors) throws Exception {
-
-        if (errors.hasErrors()) {
-            return new ModelAndView("errors.jspx", "errors", errors.getAllErrors());
-        }
-
-        UserDeleteCommand command = (UserDeleteCommand)commandObject;
-        String userID = command.getUserID();
-        String viewName = "userDelete.jsp";
-
-        ModelAndView mav = new ModelAndView(viewName);
-        mav.addObject("userID", userID);
-        return mav;
+    protected ModelAndView showForm(HttpServletRequest request,
+    		HttpServletResponse response, BindException errors,
+    		Map model) throws Exception {
+    	if (model == null) {
+    		model = new HashMap();
+    	}
+    	
+    	model.put("allUsers", service.getAllUsers());
+    	return super.showForm(request, response, errors, model);
     }
+    
+	@Override
+	protected ModelAndView onSubmit(HttpServletRequest request,
+			HttpServletResponse response, Object _command, BindException errors)
+			throws Exception {
+		UserDeleteCommand command = (UserDeleteCommand)_command;
+		Integer userId = command.getUserId();
+
+    	try {
+    		service.deleteUser(userId);
+    		return new ModelAndView(getSuccessView(), "userId", userId);
+    	} catch (Exception e) {
+    		Map<String, Object> model = new HashMap<String, Object>();
+    		model.put("command", command);
+    		model.put("message", e.getMessage());
+    		return showForm(request, response, errors, model);
+    	}
+	}
 }
--- trunk/serengeti/src/main/webapp/WEB-INF/tiles/userDelete.jsp
+++ trunk/serengeti/src/main/webapp/WEB-INF/tiles/userDelete.jsp
@@ -4,22 +4,50 @@
 <html>
     <head>
         <title>Delete a User</title>
+        
+        <style type="text/css">
+        .error {
+            padding: 5em;
+            background-color: red;
+        }
+        </style>
+        
+        <script type="text/javascript">
+            function deleteUser(id) {
+                var bodies = document.getElementsByTagName("body");
+                var form = document.createElement("form");
+                var input = document.createElement("input");
+                
+                form.action = "delete";
+                form.method = "POST";
+                
+                input.type = "hidden";
+                input.name = "userId";
+                input.value = id;
+                
+                form.appendChild(input);
+                
+                bodies[0].appendChild(form);
+                
+                form.submit();
+            }
+        </script>
     </head>
   
     <body>
-    	<c:if test='${ !empty userID }'>
-    		</p>Stub page - indicates userID <c:out value="${userID}"/> has been deleted.</p>
-    	</c:if>
+        <c:if test="${ !empty message }">
+        <p class="error"><c:out value="${message}"/></p>
+        </c:if> 
     
-		<c:if test='${ empty userID }'>    
-    	<p>Which ID would you like to delete?</p>
-        <p>
-			<form method="get" action="delete">
-				<label for="userID"> User ID </label><input type="text" name="userID"/><br/>
-				<input type="submit" value="Delete ID"/>
-			</form>
-        </p>
-        </c:if>
+    	<p>Select the user that you'd like to delete!</p>
+    
+        <ul>
+		  <c:forEach var="user" items="${allUsers}">
+		  <li>
+		      <a href="javascript:void(0);" onclick="deleteUser(${user.id});"><c:out value="${user.loginName}"/></a>
+		  </li>
+          </c:forEach>
+        </ul>
         
     </body>
 </html>
