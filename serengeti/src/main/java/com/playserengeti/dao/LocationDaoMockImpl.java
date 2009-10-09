4a5988aefa6e4580b6e0af514b89cd509db70f2a
Ray Toal <rtoal@lmu.edu>
2009-10-09 06:08:42 +0000

    Inject teamDao into LocationDao

--- serengeti/src/main/java/com/playserengeti/dao/LocationDaoMockImpl.java
+++ serengeti/src/main/java/com/playserengeti/dao/LocationDaoMockImpl.java
@@ -13,78 +13,77 @@ import com.playserengeti.domain.Location;
  */
 public class LocationDaoMockImpl implements LocationDao {
 
-	private static TeamDao teamDao;
+    // Storage for the location objects
+    private Map<Integer, Location> storage;
+    private int maxId;
 
-	// Sample teams to insert into the database.
-	private static final Location[] sampleLocations = {
-		new Location(null, "Doolan Hall", 1.3819383, 2.4928394, teamDao.getTeamById(1)),
-		new Location(null, "Viva Fresh", 3.3840183, 4.3892839, teamDao.getTeamById(1)),
-		new Location(null, "LAX Airport", 5.382728, 1.28837392, teamDao.getTeamById(1)),
-		new Location(null, "Chris's House", 1.2839282, 0.38371930, teamDao.getTeamById(1)),
-		new Location(null, "3rd Street Promenade", 12.393028, 3.3948292, teamDao.getTeamById(3)),
-		new Location(null, "ChickMagnetVille", 6.93838293, 6.93838469, teamDao.getTeamById(2)),
-	};
+    private TeamDao teamDao;
 
-	private Map<Integer, Location> storage;
-	private int maxId;
+    public LocationDaoMockImpl(TeamDao teamDao) {
+        this.teamDao = teamDao;
 
-	public LocationDaoMockImpl() {
-		storage = Collections.synchronizedMap(new HashMap<Integer, Location>());
-		maxId = -1;
+        storage = Collections.synchronizedMap(new HashMap<Integer, Location>());
+        maxId = -1;
 
-		// Insert the sample locations into the database as this is a mock impl.
-		insertLocations(sampleLocations);
-	}
+        Location[] sampleLocations = new Location[] {
+            new Location(null, "Doolan Hall", 1.3819383, 2.4928394, teamDao.getTeamById(1)),
+            new Location(null, "Viva Fresh", 3.3840183, 4.3892839, teamDao.getTeamById(1)),
+            new Location(null, "LAX Airport", 5.382728, 1.28837392, teamDao.getTeamById(1)),
+            new Location(null, "Chris's House", 1.2839282, 0.38371930, teamDao.getTeamById(1)),
+            new Location(null, "3rd Street Promenade", 12.393028, 3.3948292, teamDao.getTeamById(3)),
+            new Location(null, "ChickMagnetVille", 6.93838293, 6.93838469, teamDao.getTeamById(2)),
+        };
 
-	@Override
-	public void deleteLocation(Integer locationId) {
-		storage.remove(locationId);
-	}
+        // Insert the sample locations into the database as this is a mock impl.
+        insertLocations(sampleLocations);
+    }
 
-	@Override
-	public Collection<Location> getAllLocations() {
-		return new HashSet<Location>(storage.values());
-	}
+    @Override
+    public void deleteLocation(Integer locationId) {
+        storage.remove(locationId);
+    }
 
-	@Override
-	public Location getLocationByLocationName(String location) {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public Collection<Location> getAllLocations() {
+        return new HashSet<Location>(storage.values());
+    }
 
-	@Override
-	public Location getLocationById(Integer locationId) {
-		return new Location(storage.get(locationId));
-	}
+    @Override
+    public Location getLocationByLocationName(String location) {
+        throw new UnsupportedOperationException();
+    }
 
-	/**
-	 * A convenience method to insert an array of teams into the database.
-	 */
-	private void insertLocations(Location[] locations) {
-		for (Location location : locations) {
-			insertLocation(location);
-		}
-	}
+    @Override
+    public Location getLocationById(Integer locationId) {
+        return new Location(storage.get(locationId));
+    }
 
+    /**
+     * A convenience method to insert an array of teams into the database.
+     */
+    private void insertLocations(Location[] locations) {
+        for (Location location : locations) {
+            insertLocation(location);
+        }
+    }
 
-	@Override
-	public void updateLocation(Location location) {
-		storage.put(location.getLocationId(), location);
-	}
+    @Override
+    public void updateLocation(Location location) {
+        storage.put(location.getLocationId(), location);
+    }
 
-	@Override
-	public Integer insertLocation(Location location) {
-		Integer locationId = ++maxId;
+    @Override
+    public Integer insertLocation(Location location) {
+        Integer locationId = ++maxId;
 
-		location.setLocationId(locationId);
-		updateLocation(location);
+        location.setLocationId(locationId);
+        updateLocation(location);
 
-		return locationId;
-	}
+        return locationId;
+    }
 
-	@Override
-	public boolean locationExists(Integer locationId){
-		return (storage.get(locationId) != null);
-	}
-
+    @Override
+    public boolean locationExists(Integer locationId){
+        return (storage.get(locationId) != null);
+    }
 }
-
