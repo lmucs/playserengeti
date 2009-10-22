8ae6d1f6d275849959bac431ab2084b6190b1b44
ray.toal <ray.toal@gmail.com>
2009-10-22 22:27:07 +0000

    Improve the stub script

--- serengeti/src/test/resources/schema-init.sql
+++ serengeti/src/test/resources/schema-init.sql
@@ -1,3 +1,14 @@
+------------------------------------------------------------------------------
+-- Serengeti Postgres Database Initialization Script
+--
+-- This only works in PostgreSQL.
+--
+-- It will blow away the tables in database serengeti_dev and populate them
+-- with test data.
+------------------------------------------------------------------------------
+
+\c serengeti_dev
+
 drop table Team;
 
 create table Team (
@@ -8,6 +19,8 @@ create table Team (
     image_url text,
     constraint pk_team primary key (id)
 );
+
+insert into Team values (1, 'Constipated Koalas', 'What do you think?', 'grey', 'http://www.google.com');
 
 drop table Player;
 
@@ -22,6 +35,4 @@ insert into Player values (1, 'jcol88@gmail.com', 'James Coleman');
 insert into Player values (2, 'loren.abrams@gmail.com', 'Loren Abrams');
 insert into Player values (3, 'president@whitehouse.gov', 'Barack Obama');
 insert into Player values (4, 'mxchickmagnet86@gmail.com', 'Chick Magnet');
-
-insert into Team values (1, 'Constipated Koalas', 'What do you think?', 'grey', 'http://www.google.com');
 
