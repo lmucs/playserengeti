366cb8070bdb60e9522e848f1d70ee7776338162
ray.toal <ray.toal@acffc2da-a247-11de-8af6-edd33a9b1062>
2009-10-27 21:29:04 +0000

    Formatting

--- trunk/serengeti/src/main/java/com/playserengeti/controller/UserCommand.java
+++ trunk/serengeti/src/main/java/com/playserengeti/controller/UserCommand.java
@@ -3,68 +3,85 @@ package com.playserengeti.controller;
 import java.util.Map;
 
 public class UserCommand {
-	Integer userId;	
-    String email;	
-    String password;	
-    String displayName;
-    String image;
-    Map<Integer, String> friends;
-    Integer[] friendRemovals;
-    Map<Integer, String> teams;
-    Integer[] teamRemovals;
-    
-	public Integer getUserId() {
-		return userId;
-	}
-	public void setUserId(Integer userId) {
-		this.userId = userId;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getDisplayName() {
-		return displayName;
-	}
-	public void setDisplayName(String displayName) {
-		this.displayName = displayName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public Map<Integer, String> getFriends() {
-		return friends;
-	}
-	public void setFriends(Map<Integer, String> friends) {
-		this.friends = friends;
-	}
-	public Map<Integer, String> getTeams() {
-		return teams;
-	}
-	public void setTeams(Map<Integer, String> teams) {
-		this.teams = teams;
-	}
-	public String getImage() {
-		return image;
-	}
-	public void setImage(String image) {
-		this.image = image;
-	}
-	public Integer[] getFriendRemovals() {
-		return friendRemovals;
-	}
-	public void setFriendRemovals(Integer[] friendRemovals) {
-		this.friendRemovals = friendRemovals;
-	}
-	public Integer[] getTeamRemovals() {
-		return teamRemovals;
-	}
-	public void setTeamRemovals(Integer[] teamRemovals) {
-		this.teamRemovals = teamRemovals;
-	}
+    private Integer userId;
+    private String email;
+    private String password;
+    private String displayName;
+    private String image;
+    private Map<Integer, String> friends;
+    private Integer[] friendRemovals;
+    private Map<Integer, String> teams;
+    private Integer[] teamRemovals;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Map<Integer, String> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(Map<Integer, String> friends) {
+        this.friends = friends;
+    }
+
+    public Map<Integer, String> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(Map<Integer, String> teams) {
+        this.teams = teams;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Integer[] getFriendRemovals() {
+        return friendRemovals;
+    }
+
+    public void setFriendRemovals(Integer[] friendRemovals) {
+        this.friendRemovals = friendRemovals;
+    }
+
+    public Integer[] getTeamRemovals() {
+        return teamRemovals;
+    }
+
+    public void setTeamRemovals(Integer[] teamRemovals) {
+        this.teamRemovals = teamRemovals;
+    }
 }
