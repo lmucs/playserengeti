5b16c55b5b5a5167a23912b9512fbdc9ed9b9b3a
ray.toal <ray.toal@acffc2da-a247-11de-8af6-edd33a9b1062>
2009-10-29 06:48:27 +0000

    Add log4j to the pom and add a couple log lines, just to show how it is done

--- trunk/serengeti/src/main/java/com/playserengeti/controller/TeamController.java
+++ trunk/serengeti/src/main/java/com/playserengeti/controller/TeamController.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
@@ -15,58 +16,59 @@ import com.playserengeti.service.TeamService;
 import com.playserengeti.service.UserService;
 import com.playserengeti.session.UserSession;
 
+/**
+ * A multi-action controller for most of the team-related operations.
+ */
 public class TeamController extends MultiActionController {
-	
-	private TeamService teamService;
-	private UserService userService;
-	private UserSession session;
-	
-	public TeamController(TeamService teamService, UserService userService) {
-		this.teamService = teamService;
-		this.userService = userService;
-	}
-	
-	public ModelAndView team (HttpServletRequest request, HttpServletResponse response) {
-		return new ModelAndView("redirect:team/");
-	}
-	
-	/**
-	 * The method to be used with going to /team.
-	 * Shows interesting information regarding teams.
-	 * @param request
-	 * @param response
-	 * @return
-	 */
-	public ModelAndView central(HttpServletRequest request, HttpServletResponse response) {
-		
-		//TODO
-		Collection<Team> leaders = teamService.getAllTeams();
-		Collection<Team> mostActive = teamService.getAllTeams();
-		Collection<Team> newest = teamService.getAllTeams();
-		
-		ModelAndView mav = new ModelAndView("teamCentral");
-		mav.addObject("session", session);
-		mav.addObject("leaders", leaders);
-		mav.addObject("mostActive", mostActive);
-		mav.addObject("newest", newest);
-		
-		return mav;
-	}	
-	
-	/**
-	 * Brings up the given team's profile page.
-	 * @param request
-	 * @param response
-	 * @return
-	 */
-	public ModelAndView view(HttpServletRequest request, HttpServletResponse response) {
-		TeamCommand command = new TeamCommand();
-		Integer teamId = Integer.valueOf(request.getParameter("teamId"));
+    private Logger logger = Logger.getLogger(getClass());
+
+    private TeamService teamService;
+    private UserService userService;
+    private UserSession session;
+
+    public TeamController(TeamService teamService, UserService userService) {
+        this.teamService = teamService;
+        this.userService = userService;
+    }
+
+    public ModelAndView team(HttpServletRequest request, HttpServletResponse response) {
+        return new ModelAndView("redirect:team/");
+    }
+
+    /**
+     * Shows interesting information regarding teams.
+     */
+    public ModelAndView central(HttpServletRequest request, HttpServletResponse response) {
+        logger.debug("Entering team central");
+
+        //TODO
+        Collection<Team> leaders = teamService.getAllTeams();
+        Collection<Team> mostActive = teamService.getAllTeams();
+        Collection<Team> newest = teamService.getAllTeams();
+
+        ModelAndView mav = new ModelAndView("teamCentral");
+        mav.addObject("session", session);
+        mav.addObject("leaders", leaders);
+        mav.addObject("mostActive", mostActive);
+        mav.addObject("newest", newest);
+
+        return mav;
+    }
+
+    /**
+     * Brings up the given team's profile page.
+     */
+    public ModelAndView view(HttpServletRequest request, HttpServletResponse response) {
+        logger.debug("Entering team view");
+
+        Integer teamId = Integer.valueOf(request.getParameter("teamId"));
+
         Team team = null;
         if (teamId != null) {
             team = teamService.getTeamById(teamId);
         }
-        
+
+        TeamCommand command = new TeamCommand();
         command.setTeamId(teamId);
         command.setName(team.getName());
         command.setColor(team.getColor());
@@ -74,14 +76,16 @@ public class TeamController extends MultiActionController {
         command.setHomeBase(team.getHomeBase());
         command.setImage(team.getImage());
 
-        if (team.getLeader() != null) command.setLeaderId(team.getLeader().getUserId());
-        
-		Collection<Integer> users = teamService.getTeamMembers(teamId);
-				
-		Collection<User> members = new HashSet<User>();
-		for (Integer id : users) {
-			members.add(userService.getUserById(id));
-		}
+        if (team.getLeader() != null) {
+            command.setLeaderId(team.getLeader().getUserId());
+        }
+
+        Collection<Integer> users = teamService.getTeamMembers(teamId);
+
+        Collection<User> members = new HashSet<User>();
+        for (Integer id : users) {
+            members.add(userService.getUserById(id));
+        }
 
         ModelAndView mav = new ModelAndView("teamViewProfile");
         mav.addObject("session", session);
@@ -89,13 +93,13 @@ public class TeamController extends MultiActionController {
         mav.addObject("members", members);
 
         return mav;
-	}
-	
-	public UserSession getSession() {
-		return session;
-	}
-	
-	public void setSession(UserSession session) {
-		this.session = session;
-	}
+    }
+
+    public UserSession getSession() {
+        return session;
+    }
+
+    public void setSession(UserSession session) {
+        this.session = session;
+    }
 }
