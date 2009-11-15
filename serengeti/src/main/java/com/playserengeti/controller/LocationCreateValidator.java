98b3ca70c72b8cd82b9ab9fa683549207028ceeb
Ray Toal <rtoal@lmu.edu>
2009-11-15 23:26:44 +0000

    Latitudes can only be between -90 and 90

--- serengeti/src/main/java/com/playserengeti/controller/LocationCreateValidator.java
+++ serengeti/src/main/java/com/playserengeti/controller/LocationCreateValidator.java
@@ -5,27 +5,27 @@ import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 public class LocationCreateValidator implements Validator {
-	@Override
-	public boolean supports(Class c) {
-		return LocationCommand.class.equals(c);
-	}
+    @Override
+    public boolean supports(Class c) {
+        return LocationCommand.class.equals(c);
+    }
 
-	@Override
-	public void validate(Object object, Errors errors) {
-		LocationCommand c = LocationCommand.class.cast(object);
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "locationName",
-				"locationName.blank", "The location name can not be blank");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "latitude",
-				"latitude.blank", "The latitude can not be blank");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "longitude",
-				"longitude.blank", "The longitude can not be blank");
-		if (c.getLatitude() < -180 || c.getLatitude() > 180) {
-			errors.rejectValue("latitude", "invalid.latitude",
-					"latitudes must be between -180 and 180");
-		}
-		if (c.getLongitude() < -180 || c.getLongitude() > 180) {
-			errors.rejectValue("longitude", "invalid.longitude",
-					"longitudes must be between -180 and 180");
-		}
-	}
+    @Override
+    public void validate(Object object, Errors errors) {
+        LocationCommand c = LocationCommand.class.cast(object);
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "locationName",
+                "locationName.blank", "The location name can not be blank");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "latitude",
+                "latitude.blank", "The latitude can not be blank");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "longitude",
+                "longitude.blank", "The longitude can not be blank");
+        if (c.getLatitude() < -90 || c.getLatitude() > 90) {
+            errors.rejectValue("latitude", "invalid.latitude",
+                    "latitudes must be between -180 and 180");
+        }
+        if (c.getLongitude() < -180 || c.getLongitude() > 180) {
+            errors.rejectValue("longitude", "invalid.longitude",
+                    "longitudes must be between -180 and 180");
+        }
+    }
 }
