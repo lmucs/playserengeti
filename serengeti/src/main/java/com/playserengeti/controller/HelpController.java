2a910dbdeda1a27b28585b2954b13724ebacd811
Ray Toal <rtoal@lmu.edu>
2009-11-23 02:09:51 +0000

    Add help page stub Add new styles

---
+++ serengeti/src/main/java/com/playserengeti/controller/HelpController.java
@@ -0,0 +1,31 @@
+package com.playserengeti.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import com.playserengeti.session.UserSession;
+
+/**
+ * Controller for producing the help page for Serengeti.
+ */
+public class HelpController extends AbstractController {
+
+    private UserSession session;
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        return new ModelAndView("help", "session", session);
+    }
+
+    public UserSession getSession() {
+        return session;
+    }
+
+    public void setSession(UserSession session) {
+        this.session = session;
+    }
+}
