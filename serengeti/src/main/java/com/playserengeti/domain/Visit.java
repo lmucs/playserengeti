9190a6543faf37533f631194717c40523dfc337b
Ray Toal <rtoal@lmu.edu>
2009-12-08 23:18:24 +0000

    Format dates

--- serengeti/src/main/java/com/playserengeti/domain/Visit.java
+++ serengeti/src/main/java/com/playserengeti/domain/Visit.java
@@ -1,13 +1,15 @@
 package com.playserengeti.domain;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
  * A game visit, or check-in.
  */
 public class Visit {
+    private static SimpleDateFormat sdf = new SimpleDateFormat("MMM dd, yyyy hh:mma");
 
-	private Integer id;
+    private Integer id;
     private User user;
     private Team team;
     private Location location;
@@ -15,10 +17,10 @@ public class Visit {
 
     public Visit() {
     }
-    
+
     public Visit(Visit b) {
-    	this(b.id, b.user, b.team, b.location);
-    	this.date = b.date;
+        this(b.id, b.user, b.team, b.location);
+        this.date = b.date;
     }
 
     public Visit(Integer id, User user, Team team, Location location) {
@@ -29,88 +31,91 @@ public class Visit {
         this.date = new Date();
     }
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public User getUser() {
-		return user;
-	}
+    public User getUser() {
+        return user;
+    }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
+    public void setUser(User user) {
+        this.user = user;
+    }
 
-	public Location getLocation() {
-		return location;
-	}
+    public Location getLocation() {
+        return location;
+    }
 
-	public void setLocation(Location location) {
-		this.location = location;
-	}
+    public void setLocation(Location location) {
+        this.location = location;
+    }
 
-	public Team getTeam() {
-		return team;
-	}
+    public Team getTeam() {
+        return team;
+    }
 
-	public void setTeam(Team team) {
-		this.team = team;
-	}
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 
-	public Date getDate() {
-		return date;
-	}
+    public Date getDate() {
+        return date;
+    }
 
-	public void setDate(Date date) {
-		this.date = date;
-	}
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
-	public String asJSON() {
-		return "{\"id\" : \"" + this.id + "\", \"user\" : " + this.user.asMinimalJSON() + ", \"team\" : " + this.team.asMinimalJSON() + 
-		    ", \"location\" : " + this.location.asMinimalJSON() + ", \"date\" : \"" + this.date.toString() + "\"}";
-	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result
-				+ ((location == null) ? 0 : location.hashCode());
-		result = prime * result + ((user == null) ? 0 : user.hashCode());
-		return result;
-	}
+    public String asJSON() {
+        return "{\"id\" : \"" + this.id +
+            "\", \"user\" : " + this.user.asMinimalJSON() +
+            ", \"team\" : " + this.team.asMinimalJSON() +
+            ", \"location\" : " + this.location.asMinimalJSON() +
+            ", \"date\" : \"" + sdf.format(this.date) + "\"}";
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Visit other = (Visit) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (location == null) {
-			if (other.location != null)
-				return false;
-		} else if (!location.equals(other.location))
-			return false;
-		if (user == null) {
-			if (other.user != null)
-				return false;
-		} else if (!user.equals(other.user))
-			return false;
-		return true;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result
+                + ((location == null) ? 0 : location.hashCode());
+        result = prime * result + ((user == null) ? 0 : user.hashCode());
+        return result;
+    }
 
-	// TODO: toString
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Visit other = (Visit) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (location == null) {
+            if (other.location != null)
+                return false;
+        } else if (!location.equals(other.location))
+            return false;
+        if (user == null) {
+            if (other.user != null)
+                return false;
+        } else if (!user.equals(other.user))
+            return false;
+        return true;
+    }
+
+    // TODO: toString
 }
